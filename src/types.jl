9b2f7a62f6419f6d95b20c780acc1d20fea61c9f
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2019-04-23 16:02:36 -0400

    Finish logic and types for CUSUM and EWMA

--- src/types.jl
+++ src/types.jl
@@ -1,48 +1,229 @@
-# This file is part of ControlCharts.jl.
+# This file is part of ControlSeriess.jl.
 #
-# ControlCharts.jl is free software: you can redistribute it and/or modify it
+# ControlSeriess.jl is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 #
-# ControlCharts.jl is distributed in the hope that it will be useful, but
+# ControlSeriess.jl is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 
 module Types
 
-export ShewartChart, CumulativeSumChart, CUSUM,
-    ExponentiallyWeightedMovingAverageChart, EWMA
+export ControlChart, calculate,
+    TabularCumulativeSum, CUSUM, ExponentiallyWeightedMovingAverage, EWMA
 
-abstract type ControlChart end
+import Statistics: mean, std
 
-abstract type ShewartChart <: ControlChart end
+"Represents a series of values to be turned into a control chart."
+abstract type ControlSeries{T,V} end
 
-abstract type CumulativeSumChart <: ControlChart end
-const CUSUM = CumulativeSumChart
+"""
+    ControlChart(z, lcl, ucl, control_series)
 
-struct ExponentiallyWeightedMovingAverageChart{T, V<:AbstractVector{T}} <: ControlChart
-    λ::Float64
-    μ::T
+Represents a control chart.
+
+Vectors in tuple `x` is the main series to plot while `lcl` and `ucl` are
+vectors defining points where each point in each vector in `x` must
+not exceed in the negative and positive directions. This function is
+returned by calling `calculate` on a `ControlSeries`.
+"""
+struct ControlChart{T, V <: AbstractVector{T}, C <: ControlSeries{T,V},
+                    N, Z <: NTuple{N,V}}
+    z::Z
+    LCL::V
+    UCL::V
+    control_series::C
+end#struct
+
+"""
+    ControlChart(c)
+
+Calls `calculate` on control series `c`.
+"""
+ControlChart(c::ControlSeries) = calculate(c)
+
+"""
+    calculate(control_series)
+
+Returns a `ControlChart` for a given series.
+"""
+function calculate end
+
+# Shewart Charts
+#---------------
+
+abstract type ShewartSeries{T,V} <: ControlSeries{T,V} end
+
+# Cumulative Sum Charts (CUSUM)
+#------------------------------
+
+abstract type CumulativeSum{T,V} <: ControlSeries{T,V} end
+
+struct TabularCumulativeSum{T, V<:AbstractVector{T}} <: CumulativeSum{T,V}
     x::V
-    function ExponentiallyWeightedMovingAverageChart(
-        λ::Float64, μ::T, x::V
+    K::T
+    H::T
+    μ::T
+end#struct
+
+"""
+    CUSUM(x, K, H, μ)
+
+Create tabular Cumulative Sum series on variable `x`.
+
+`K` is the "reference value" (or "allowance" or "slack value"), `H` is
+the "decision interval" (to establish control limits) and `μ` is the
+target value.
+
+# References
+
+- Introduction to Statistical Quality Control
+  - Chapter
+    - 9.1.2: The Tabular or Algorithmic CUSUM for Monitoring the Process Mean
+  - Pages
+    - 417--421
+  - Author:
+    - Montgomery, Douglas C.
+  - ISBN:
+    - 978-1-118-14681-1
+  - Publisher
+    - John Wiley & Sons, Inc.
+  - Year
+    - 2013
+"""
+const CUSUM = TabularCumulativeSum
+
+function CUSUM(x::AbstractVector{V}, k::K, h::H, μ::MU) where {V, K, H, MU}
+    T = promote_type(promote_type(promote_type(V, K), H), MU)
+    xt = similar(x, T)
+    xt .= x
+    CUSUM(x::AbstractVector{T},
+          convert(T, k)::T, convert(T, h), convert(T, μ)::T)
+end#constructor
+
+function calculate(series::CUSUM{T,V}) where {T,V}
+    cpositive = similar(series.x)
+    cnegative = similar(series.x)
+
+    cpositive[1] = max(zero(T), series.x[1] - series.μ - series.K)
+    cnegative[1] = min(zero(T), series.x[1] - series.μ + series.K)
+    for i in 2:length(cpositive)
+        cpositive[i] = max(zero(T),
+                           series.x[i] - series.μ - series.K + cpositive[i-1])
+        cnegative[i] = min(zero(T),
+                           series.x[i] - series.μ + series.K + cnegative[i-1])
+    end#for
+
+    lcl = similar(series.x)
+    ucl = similar(series.x)
+    lcl .= Ref(-series.H)
+    ucl .= Ref(+series.H)
+
+    ControlChart((cnegative, cpositive), lcl, ucl, series)
+end#function
+
+# Exponentially Weighted Moving Average Chart (EWMA)
+#---------------------------------------------------
+
+struct ExponentiallyWeightedMovingAverage{T, V<:AbstractVector{T}} <: ControlSeries{T,V}
+    x::V
+    λ::Float64
+    L::Float64
+    μ::T
+    σ::T
+    function ExponentiallyWeightedMovingAverage(
+        x::V, λ::Float64, l::Float64, μ::T, σ::T
     ) where {T, V<:AbstractVector{T}}
-        if 0 < λ <= 1
-            new{T, V}(λ, μ, x)
-        else
+        if λ <= 0 || λ > 1
             throw(DomainError(λ, "Weight must be 0 < λ <= 1"))
+        elseif l <= 0
+            throw(DomainError(l, "Control limit width must be positive"))
+        elseif σ < 0
+            throw(DomainError(σ, "Standard deviation must be positive"))
+        else
+            new{T, V}(x, λ, l, μ, σ)
         end#if
     end#constructor
 end#struct
-const EWMA = ExponentiallyWeightedMovingAverageChart
 
-function EWMA(λ, μ::T1, x::AbstractVector{T2}) where {T1, T2}
+"""
+    EWMA(x, λ, l, μ, σ)
+
+Represents an exponentially weighted moving average series on `x`.
+
+Each point `z` in the EWMA is calculated according to:
+
+```
+z[0] = μ
+z[i] = λ * x[i] + (1 - λ) * z[i-1]
+```
+
+where `λ` is a constant.
+
+The LCL and UCL are calculated according to:
+
+```
+cl[i] = L * σ * sqrt(λ * (1 - (1 - λ)^(2i)) / (2 - λ))
+LCL[i] = μ - cl[i]
+UCL[i] = μ + cl[i]
+```
+
+where `L` is a constant, `σ` is the standard deviation of `x`, and `μ`
+is the process target.
+
+# References
+
+- Introduction to Statistical Quality Control
+  - Chapter
+    - 9.2: The Exponentially Weighted Moving Average Control Chart
+  - Pages
+    - 433--442
+  - Author:
+    - Montgomery, Douglas C.
+  - ISBN:
+    - 978-1-118-14681-1
+  - Publisher
+    - John Wiley & Sons, Inc.
+  - Year
+    - 2013
+"""
+const EWMA = ExponentiallyWeightedMovingAverage
+
+# try to make sure EWMA works even if types aren't as expected
+function EWMA(
+    x::AbstractVector{T2}, λ::Real, l::Real, μ::T1, σ::T1
+) where {T1, T2}
     T = promote_type(T1, T2)
     xt = similar(x, T)
     xt .= x
-    EWMA(Float64(λ), T(μ), xt)
+    EWMA(xt, convert(Float64, λ), convert(Float64, l),
+         convert(T, μ), convert(T, σ))
+end#constructor
+
+"""
+    EWMA(x; λ=0.2, l=3.0, μ=mean(x), σ=std(x))
+"""
+EWMA(x::AbstractVector; λ=0.2, L=3.0, μ=mean(x), σ=std(x)) = EWMA(x, λ, L, μ, σ)
+
+function calculate(cs::EWMA)
+    z = similar(cs.x)
+
+    z[1] = cs.λ * cs.x[1] + (1 - cs.λ)cs.μ
+    for i in 2:length(z)
+        z[i] = cs.λ * cs.x[i] + (1 - cs.λ)*z[i-1]
+    end#for
+
+    lcl = similar(cs.x)
+    ucl = similar(cs.x)
+    let σ = cs.σ, λ = cs.λ, l = cs.L, μ = cs.μ
+        cl = (l*σ*sqrt(λ*(1 - (1 - λ)^(2i)) / (2 - λ)) for i in 1:length(lcl))
+        lcl .= Ref(μ) .- cl
+        ucl .= Ref(μ) .+ cl
+    end#let
+    ControlChart((z,), lcl, ucl, cs)
 end#function
 
 end#module Types
--- test/types.jl
+++ test/types.jl
@@ -13,8 +13,90 @@
 using ControlCharts.Types
 
 @testset "Constructors" begin
-    @test EWMA(0.5, 2, [1,2,3,4]).λ ≈ 0.5
-    @test_throws DomainError EWMA(1.1, 2, [1,2,3,4])
-    @test_throws DomainError EWMA(0, 2, [1,2,3,4])
-    @test_throws DomainError EWMA(-5, 2, [1,2,3,4])
-end
+    @test_throws DomainError EWMA([1,2,3,4], 1.1, 3, 2, 1)
+    @test_throws DomainError EWMA([1,2,3,4], 2, 3, 2, 1)
+    @test_throws DomainError EWMA([1,2,3,4], -5, 3, 2, 1)
+    @test_throws DomainError EWMA([1,2,3,4], 0.5, -3, 2, 1)
+    @test_throws DomainError EWMA([1,2,3,4], 0.5, 3, 2, -1)
+end#@testset
+
+const x = [09.45,
+           07.99,
+           09.29,
+           11.66,
+           12.16,
+           10.18,
+           08.04,
+           11.46,
+           09.20,
+           10.34]
+const cpositive = [0,
+                   0,
+                   0,
+                   1.16,
+                   2.82,
+                   2.50,
+                   0.04,
+                   1.00,
+                   0,
+                   0]
+const cnegative = -[0.05,
+                    1.56,
+                    1.77,
+                    0,
+                    0,
+                    0,
+                    1.46,
+                    0,
+                    0.30,
+                    0]
+const cusum = CUSUM(x, 0.5, 5, 10)
+const cusum_chart = calculate(cusum)
+const cusum_chart2 = ControlChart(cusum)
+
+@testset "CUSUM" begin
+    @test all(cusum_chart.z[1] .≈ cnegative)
+    @test all(cusum_chart.z[2] .≈ cpositive)
+    @test all(cusum_chart.LCL .≈ -5)
+    @test all(cusum_chart.UCL .≈ 5)
+    @test all(cusum_chart.z[1] .≈ cusum_chart2.z[1])
+end#@testset
+
+const z_ewma = [09.94500,
+                09.74950,
+                09.70355,
+                09.89920,
+                10.12530,
+                10.13070,
+                09.92167,
+                10.07550,
+                09.98796,
+                10.02320]
+const cl_ewma = [0.27,
+                 0.363248,
+                 0.424003,
+                 0.467462,
+                 0.499902,
+                 0.52471,
+                 0.543976,
+                 0.559095,
+                 0.571048,
+                 0.580549]
+
+const ewma = EWMA(x, 0.1, 2.7, 10, 1)
+const ewma_chart = calculate(ewma)
+
+@testset "EWMA" begin
+    @test all(isapprox.(ewma_chart.z[1], z_ewma, atol=0.0001))
+    @test all(isapprox.(ewma_chart.LCL, 10 .- cl_ewma, atol=0.0001))
+    @test all(isapprox.(ewma_chart.UCL, 10 .+ cl_ewma, atol=0.0001))
+    # test kwarg form of EWMA
+    @test EWMA([1,2,3]).λ ≈ 0.2
+    @test EWMA([1,2,3]; λ=0.15).λ ≈ 0.15
+    @test EWMA([1,2,3]).L ≈ 3
+    @test EWMA([1,2,3]; L=4).L ≈ 4
+    @test EWMA([1,2,3]).μ ≈ 2
+    @test EWMA([1,2,3]; μ=2.5).μ ≈ 2.5
+    @test EWMA([1,2,3]).σ ≈ 1
+    @test EWMA([1,2,3]; σ=1.1).σ ≈ 1.1
+end#@testset
